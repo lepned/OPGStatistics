6b7b64c358245315c37549f8a3cc51100722f74b
lepned <lepned@yahoo.com>
2015-03-14 17:52:52 +0100

    First commit 14.03.2015

---
+++ OsloPubGuideStatistikk/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open FSharp.Data
+
+
+
+
+
+//let fil = @"C:\Users\lepne_000\Downloads\OpgData.json"
+type opg = JsonProvider<"C:\Users\lepne_000\Downloads\OpgData.json">
+let pubs = opg.GetSamples()
+
+let extract =
+    let report = ref String.Empty
+    pubs
+    |>Seq.map(fun e -> String.length(e.JsonValue.ToString()), e.Navn, e.SistOppdatert, e.Hovedområde)
+    |>Seq.sortBy(fun (_,_,_,o) -> o)
+    //|>Seq.choose(fun (txt,navn,tid) -> tid.DateTime.IsSome)
+    |>Seq.mapi(fun idx (txt,navn,tid,o) -> sprintf "%d, %d, %s, %s, %A" (idx+1) txt navn o tid.JsonValue)
+    |>Seq.iter(fun e -> report.Value <- (report.Value + e + "\n"))
+    report
+
+let stop = 1
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
